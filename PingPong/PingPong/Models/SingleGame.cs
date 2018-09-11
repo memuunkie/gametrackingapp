f32ec24ee45d07030188c282aabcfe0d1da7e289
REM-MALCAIDE-LT\malcaide <malcaide@stackoverflow.com>
2018-09-11 16:51:52 -0700

    Class: initial commit of SingleGame

---
+++ PingPong/PingPong/Models/SingleGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace PingPong.Models
+{
+    public class SingleGame
+    {
+        public static SqlConnection _conn = new SqlConnection(ConfigurationManager.ConnectionStrings["PingPong"].ConnectionString);
+
+        public int Id { get; set; }
+        public int Player1Id { get; set; }
+        public int Player2Id { get; set; }
+        public int Player1Score { get; set; }
+        public int Player2Score { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        // set the CreationDate to now
+        public SingleGame()
+        {
+            this.CreationDate = DateTime.Now;
+        }
+
+        public static List<SingleGame> Get()
+        {
+            return _conn.Query<SingleGame>("SELECT * FROM SingleGames").ToList();
+        }
+    }
+}
