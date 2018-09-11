32c381435b7a3998bc9516d978e079542a05f48c
REM-MALCAIDE-LT\malcaide <malcaide@stackoverflow.com>
2018-09-11 12:08:47 -0700

    DataModel: Updated queries to match guidelines and put primary/foreign key constraints outside table build

--- PingPong/DataModel.sql
+++ PingPong/DataModel.sql
@@ -1,28 +1,43 @@
 CREATE TABLE Players (
-    Id int not null primary key identity(1,1),
-    LastName varchar(255) not null,
-    FirstName varchar(255) not null,
+    Id int not null identity(1,1),
+    LastName nvarchar(100) not null,
+    FirstName nvarchar(100) not null,
 	CreationDate datetime not null,
 );
+
+ALTER TABLE [Players]
+ADD CONSTRAINT [PK_Players] PRIMARY KEY (Id)
 
 -- constraints to make sure same player/team not added twice
 
 CREATE TABLE Teams (
-    Id int not null primary key identity(1,1),
-	Player1Id int not null foreign key references Players(Id),
-	Player2Id int not null foreign key references Players(Id),
-	TeamName varchar(255) unique not null,
+    Id int not null identity(1,1),
+	TeamName nvarchar(100) not null,
 	CreationDate datetime not null,
+	Player1Id int not null,
+	Player2Id int not null,
 
 	constraint TeamPlayers_Unique check (
 		Player1Id <> Player2Id
     )
 );
 
+ALTER TABLE [Teams]
+ADD CONSTRAINT [PK_Teams] PRIMARY KEY (Id)
+
+ALTER TABLE [Teams]
+ADD CONSTRAINT [PK_Teams_Player1] FOREIGN KEY (Player1Id) REFERENCES Players (Id)
+
+ALTER TABLE [Teams]
+ADD CONSTRAINT [PK_Teams_Player2] FOREIGN KEY (Player2Id) REFERENCES Players (Id)
+
+ALTER TABLE [Teams]
+ADD CONSTRAINT [Unique_TeamName] UNIQUE (TeamName)
+
 CREATE TABLE SingleGames (
-	Id int not null primary key identity(1,1),
-	Player1Id int not null foreign key references Players(Id),
-	Player2Id int not null foreign key references Players(Id),
+	Id int not null identity(1,1),
+	Player1Id int not null,
+	Player2Id int not null,
 	Player1Score int not null,
 	Player2Score int not null,
 	CreationDate datetime not null,
@@ -32,10 +47,19 @@ CREATE TABLE SingleGames (
 	)
 );
 
+ALTER TABLE [SingleGames]
+ADD CONSTRAINT [PK_SingleGames] PRIMARY KEY (Id)
+
+ALTER TABLE [SingleGames]
+ADD CONSTRAINT [PK_SGame_Player1] FOREIGN KEY (Player1Id) REFERENCES Players (Id)
+
+ALTER TABLE [SingleGames]
+ADD CONSTRAINT [PK_SGame_Player2] FOREIGN KEY (Player2Id) REFERENCES Players (Id)
+
 CREATE TABLE TeamGames (
-	Id int not null primary key identity(1,1),
-	Team1Id int not null foreign key references Teams(Id),
-	Team2Id int not null foreign key references Teams(Id),
+	Id int not null identity(1,1),
+	Team1Id int not null,
+	Team2Id int not null,
 	Team1Score int not null,
 	Team2Score int not null,
 	CreationDate datetime not null,
@@ -45,4 +69,12 @@ CREATE TABLE TeamGames (
 	)
 );
 
+ALTER TABLE [TeamGames]
+ADD CONSTRAINT [PK_TeamGames] PRIMARY KEY (Id)
+
+ALTER TABLE [TeamGames]
+ADD CONSTRAINT [PK_TGame_Team1] FOREIGN KEY (Team1Id) REFERENCES Teams (Id)
+
+ALTER TABLE [TeamGames]
+ADD CONSTRAINT [PK_TGame_Team2] FOREIGN KEY (Team2Id) REFERENCES Teams (Id)
 
