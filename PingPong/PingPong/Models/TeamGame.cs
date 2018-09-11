95e20c6f97c098bfaab937d62217642017798d47
REM-MALCAIDE-LT\malcaide <malcaide@stackoverflow.com>
2018-09-11 16:56:38 -0700

    Class: initial commit of TeamGame

---
+++ PingPong/PingPong/Models/TeamGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace PingPong.Models
+{
+    public class TeamGame
+    {
+        public static SqlConnection _conn = new SqlConnection(ConfigurationManager.ConnectionStrings["PingPong"].ConnectionString);
+
+        public int Id { get; set; }
+        public int Team1Id { get; set; }
+        public int Team2Id { get; set; }
+        public int Team1Score { get; set; }
+        public int Team2Score { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        // set the CreationDate to now
+        public TeamGame()
+        {
+            this.CreationDate = DateTime.Now;
+        }
+
+        public static List<TeamGame> Get()
+        {
+            return _conn.Query<TeamGame>("SELECT * FROM TeamGames").ToList();
+        }
+    }
+}
