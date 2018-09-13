a28ed018f11c6a3299fd1ff2cdea2c422c88b189
REM-MALCAIDE-LT\malcaide <malcaide@stackoverflow.com>
2018-09-13 08:45:18 -0700

    ViewModel: renamed to GameViewModel for both game types

---
+++ PingPong/PingPong/ViewModel/GameViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PingPong.Models;
+
+namespace PingPong.ViewModel
+{
+    public class GameViewModel
+    {
+        public List<TeamGame> TeamGames { get; set; }
+
+        public List<SingleGame> SingleGames { get; set; }
+    }
+}
